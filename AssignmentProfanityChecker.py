673d3c7f54d3daddea773f300e74f2e59ae4cc09
Andy <duckworth.andy@gmail.com>
2022-02-04 09:48:17 -0800

    Added if statements for file types.

--- AssignmentProfanityChecker.py
+++ AssignmentProfanityChecker.py
@@ -1,7 +1,36 @@
 import argparse
+import docx2txt
+from pdfminer.high_level import extract_text
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename',type=argparse.FileType('r'))
+parser.add_argument('filename')
 args = parser.parse_args()
+filename = args.filename
 
-print(args.filename.readlines())
+def profanityCheck(text2parse):
+    pass
+
+def filterTxt():
+    openFile = open(filename,'r')
+    textRead = openFile.readlines()
+    print(textRead)
+
+def filterDocx():
+    text = docx2txt.process(filename)
+    print(text)
+
+def filterPdf():
+    text = extract_text(filename)
+    print(text)
+
+def filterXlsx():
+    print(filename)
+
+if filename.endswith('.txt'):
+    filterTxt()
+elif filename.endswith('.docx') or filename.endswith('.doc'):
+    filterDocx()
+elif filename.endswith('.pdf'):
+    filterPdf()
+elif filename.endswith('xlsx') or filename.endswith('.xls'):
+    filterXlsx()
