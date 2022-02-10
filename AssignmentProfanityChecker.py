5a528d941a2ed10fe09871607caa00a1e1c4780f
Andy <duckworth.andy@gmail.com>
2022-02-10 09:46:10 -0800

    Function reorganization.

--- AssignmentProfanityChecker.py
+++ AssignmentProfanityChecker.py
@@ -2,47 +2,40 @@ import argparse
 import docx2txt
 from pdfminer.high_level import extract_text
 
-# adds an argument to the program requiring a file name at run.
-parser = argparse.ArgumentParser()
-parser.add_argument('filename')
-args = parser.parse_args()
-filename = args.filename
+def main():
+    # adds an argument to the program requiring a file name at run.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    filename = args.filename
 
-# opens file with profanity and turns it into a profanity word list.
-profanityFile = open('WordList.txt','r')
-words = profanityFile.readlines()
-wordList = [x[:-1] for x in words]
+    assignmentText = processFileType(filename)
+    checkProfanity(assignmentText)
 
-# function to check file for profanity.
-def profanityCheck(text2parse):
-    pass
+def processFileType(filename):
+    # if statements to check incoming file for filetype.
+    if filename.endswith('.txt'):
+        openFile = open(filename,'r')
+        assignmentText = openFile.readlines()
+        # print(assignmentText)
+    elif filename.endswith('.docx') or filename.endswith('.doc'):
+        assignmentText = docx2txt.process(filename)
+        print(assignmentText)
+    elif filename.endswith('.pdf'):
+        assignmentText = extract_text(filename)
+        print(assignmentText)
+    elif filename.endswith('xlsx') or filename.endswith('.xls') or filename.endswith('.csv'):
+        print(filename)
 
-# function to process plain text files.
-def filterTxt():
-    openFile = open(filename,'r')
-    textRead = openFile.readlines()
-    print(textRead)
+    return assignmentText
 
-# function to process Word documents.
-def filterDocx():
-    text = docx2txt.process(filename)
-    print(text)
+def checkProfanity(assignmentText):
+    # opens file with profanity and turns it into a profanity word list.
+    profanityFile = open('WordList.txt','r')
+    words = profanityFile.readlines()
+    wordList = [x[:-1] for x in words]
 
-# function to process PDF files.
-def filterPdf():
-    text = extract_text(filename)
-    print(text)
+    print(assignmentText)
 
-# function to process Excel or CSV files.
-def filterXlsx():
-    print(filename)
-
-# if statements to check incoming file for filetype.
-if filename.endswith('.txt'):
-    filterTxt()
-elif filename.endswith('.docx') or filename.endswith('.doc'):
-    filterDocx()
-elif filename.endswith('.pdf'):
-    filterPdf()
-elif filename.endswith('xlsx') or filename.endswith('.xls') or filename.endswith('.csv'):
-    filterXlsx()
+if __name__ == '__main__':
+    main()
